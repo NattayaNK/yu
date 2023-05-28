e0f07823bd2422d6b96f5f740f6b1ae4bac87061
NattayaNK <124546131+NattayaNK@users.noreply.github.com>
2023-05-28 21:46:39 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,165 @@
+var tourpoint ={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "1.ดอยอินทนนท์",
+        "type": "natural🌳🍃🕊",
+        "level": "9 👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          98.48692185356299,
+          18.590855727247785
+        ],
+        "type": "Point"
+      },
+      "id": 2
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "2.ม่อนแจ่ม",
+        "type": "natural🌳🍃🕊",
+        "level": "9 👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          98.82269935356078,
+          18.935723473594663
+        ],
+        "type": "Point"
+      },
+      "id": 1
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "3.วัดร่องขุน",
+        "type": "culture🛕🙌",
+        "level" : "8 👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          99.7634273535611,
+          19.82427048045338
+        ],
+        "type": "Point"
+      },
+      "id": 2
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "4.อุทยานประวัติศาสตร์พนมรุ้ง",
+        "type": "culture🛕🙌",
+        "level": "8 👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          102.94242688566203,
+          14.532001536066119
+        ],
+        "type": "Point"
+      },
+      "id": 3
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "5.สนามฟุตบอลช้างอารีนา",
+        "type": "special🚩⚽⛳",
+        "level": "8 👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          103.09520079069699,
+          14.96586617726912
+        ],
+        "type": "Point"
+      },
+      "id": 4
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "6.น้ำตกตาดโตน",
+        "type": "natural🌳🍃🕊",
+        "level": "9 👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          102.03624715171043,
+          15.981764448297383
+        ],
+        "type": "Point"
+      },
+      "id": 5
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "7.เกาะพีพี",
+        "type": "natural🌳🍃🕊",
+        "level": "10 👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          98.76632478935875,
+          7.6846130860328685
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "8.สยามพารากอน",
+        "type": "special 💸💸💰",
+        "level": "8  👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          100.53494861961025,
+          13.746889243338444
+        ],
+        "type": "Point"
+      },
+      "id": 9
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "9.อุทยานประวัติศาสตร์สุโขทัย",
+        "type": "culture🛕🙌",
+        "level": "9 👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          99.70368930541179,
+          17.020763277108017
+        ],
+        "type": "Point"
+      },
+      "id": 8
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "10.เกาะราชา",
+        "type": "natural🌳🍃🕊",
+        "level": "10 👍💖"
+      },
+      "geometry": {
+        "coordinates": [
+          98.37448708751259,
+          7.606710436480441
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
+          
+        
